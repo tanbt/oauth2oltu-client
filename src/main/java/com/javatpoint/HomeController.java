82e4894bb1adbffd353937e29b3e4889f07d418f
Tan Bui <trungtanbui@gmail.com>
2017-10-15 11:17:47 +0300

    Add a hard-code login process

--- src/main/java/com/javatpoint/HomeController.java
+++ src/main/java/com/javatpoint/HomeController.java
@@ -1,17 +1,39 @@
 package com.javatpoint;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
-	public ModelAndView mymethod(){
-		return new ModelAndView("home","msg","Hello First Spring");
-	}
-	
-	
-	
+    @RequestMapping("/")
+    public ModelAndView getHome() {
+        return new ModelAndView("home", "msg",
+                "Welcome to Client " + "Website.");
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView getLogin() {
+        return new ModelAndView("login", "msg", "Hello First Spring");
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView postLogin(HttpServletRequest request) {
+        String userName = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (userName .equals("demo") && password.equals("demo")) {
+            return new ModelAndView("home", "msg", "Welcome, " + userName
+                    + ".");
+        }
+
+        return new ModelAndView("home", "msg", "Please <a href=\"/login\">" +
+                " try again</a>.");
+    }
+
+
 }
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,49 @@
+<header>
+	<jsp:include page="header.jsp" />
+</header>
+
+<br>
+<br>
+<div class="container">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+			<div class="form-body">
+				<ul class="nav nav-tabs final-login">
+					<li class="active"><a data-toggle="tab" href="#sectionA">Sign
+							In</a></li>
+					<li><a data-toggle="tab" href="#sectionB">Join us!</a></li>
+				</ul>
+				<div class="tab-content">
+					<div id="sectionA" class="tab-pane fade in active">
+						<div class="innter-form">
+							<form class="sa-innate-form" method="post"
+								  action="/login">
+								<label>Email Address</label> <input
+									type="text" name="username" value="demo">
+								<label>Password</label> <input
+									type="password" name="password"
+									value="demo">
+								<button type="submit">Sign In</button>
+								<a href="">Forgot Password?</a>
+							</form>
+						</div>
+						<div class="social-login">
+							<p>- - - - - - - - - - - - - Sign In With - - - - - - - - - -
+								- - -</p>
+							<ul>
+								<li style="width: 100%"><a href=""><i
+										class="fa fa-facebook"></i> My OAuth 2 </a></li>
+							</ul>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<footer>
+	<jsp:include page="footer.jsp" />
+</footer>
