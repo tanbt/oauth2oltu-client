01d2d956f7bb1d82057f98eb9e198810f728a982
Tan Bui <trungtanbui@gmail.com>
2017-10-18 22:28:30 +0300

    Update default oauth url for login button

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(".oauth-button-code").click(function () {
-        var oauthUrlCode = "http://localhost:8080/oauth2/login?redirect_uri=http://localhost:8081/loginByOauth&scope=read_ekycclaims&response_type=code&client_id=clientid";
+        var oauthUrlCode = "http://localhost:8080/oauth2/login?redirect_uri=http://localhost:8081/loginByOauth&scope=user:read&response_type=code&client_id=testid1234";
         // window.open(oauthUrl, 'targetWindow',
         //     'toolbar=no, location = no, status = no, menubar = no, scrollbars = yes, esizable = yes, width = 500px, height = 500px');
         $( location ).attr("href", oauthUrlCode);
@@ -8,7 +8,7 @@ $(document).ready(function() {
     });
 
     $(".oauth-button-token").click(function () {
-        var oauthUrlToken = "http://localhost:8080/oauth2/login?redirect_uri=http://localhost:8081/loginByOauth&scope=read_ekycclaims&response_type=token&client_id=clientid";
+        var oauthUrlToken = "http://localhost:8080/oauth2/login?redirect_uri=http://localhost:8081/loginByOauth&scope=user:read&response_type=token&client_id=testid1234";
         $( location ).attr("href", oauthUrlToken);
         return false;
     });
