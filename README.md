fdb349daa7f0906c58c842bad64cfc0b9de70093
Tan Bui <trungtanbui@gmail.com>
2017-12-11 14:27:13 +0200

    Use authentication code to get Access Token
    Use apache httpclient for Rest Client

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ A simplest client website to demo authentication with oauth2oltu
 
 ## The Oauth2 Authentication server
 * https://github.com/tanbt/oauth2oltu
+
+## References
+* Rest client: https://hc.apache.org/httpcomponents-client-4.3.x/quickstart.html
+    * http://hc.apache.org/httpcomponents-client-ga/fluent-hc/apidocs/org/apache/http/client/fluent/Request.html
+    * https://hc.apache.org/httpcomponents-client-ga/tutorial/html/fluent.html
+    * Not recommended http://square.github.io/okhttp, which can cause **empty request body**
+
--- src/main/java/com/javatpoint/HomeController.java
+++ src/main/java/com/javatpoint/HomeController.java
@@ -1,14 +1,36 @@
 package com.javatpoint;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+
 @Controller
 public class HomeController {
+
+    private static  final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
+
+    private static final String HOST_TOKEN_URL =
+            "http://localhost:8080/oauth/token";
+
+    private static final String CLIENT_REDIRECT_URL =
+            "http://localhost:8081/loginByOauth";
+
+    private static final String CLIENT_ID = "testid1234";
+    private static final String CLIENT_SECRET = "testid1234secret";
 
     @RequestMapping("/")
     public ModelAndView getHome() {
@@ -23,11 +45,22 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/loginByOauth", method = RequestMethod.GET)
-    public ModelAndView getLoginOauth() {
-        // todo: check oauth query parameter
-        // todo: get access code then exchange to access token
-        // todo: get resource
-        return new ModelAndView("welcome", "msg", "Hello Login from Oauth");
+    public ModelAndView getLoginOauth(HttpServletRequest request) {
+        String oauthCode = request.getParameter("code");
+        if (oauthCode != null) {
+            JsonObject respondedJson = getAccessToken(oauthCode);
+            if (respondedJson == null) {
+                return new ModelAndView("welcome", "msg",
+                        "Cannot get Access Token");
+            }
+            String accessToken = respondedJson.get("access_token").getAsString();
+
+
+            return new ModelAndView("welcome", "msg", accessToken);
+        } else {
+            return new ModelAndView("welcome", "msg", "Missing " +
+                    "authentication code");
+        }
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
@@ -44,5 +77,24 @@ public class HomeController {
                 " try again</a>.");
     }
 
-
+    // Based on API Documentation, responding data is always JSON.
+    private JsonObject getAccessToken(String code) {
+        try {
+            String response = Request.Post(HOST_TOKEN_URL)
+                    .bodyForm(
+                            Form.form()
+                                    .add("redirect_uri",  CLIENT_REDIRECT_URL)
+                                    .add("grant_type", "authorization_code")
+                                    .add("code", code)
+                                    .add("client_id", CLIENT_ID)
+                                    .add("client_secret", CLIENT_SECRET)
+                                    .build())
+                    .execute().returnContent().asString();
+            JsonParser parser = new JsonParser();
+            return parser.parse(response).getAsJsonObject();
+        } catch (IOException e) {
+            LOGGER.warn(e.getMessage());
+            return null;
+        }
+    }
 }
